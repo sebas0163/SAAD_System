b266fe9ec90d04c04558d67c01736a0e953d413d
Sebas <sebas0163@estudiantec.cr>
2024-10-13 23:02:26 -0600

    [Add] Basic device and app comunication

--- SAAD_System/components/molecules/Metrics_View.tsx
+++ SAAD_System/components/molecules/Metrics_View.tsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import TextHolder from "@/components/atoms/TextHolder";
 
+const ESP32_URL= 'http://192.168.100.13:80/data';
+
 export default function MetricsView(){
+    const [data, setData] = useState<{ pulsaciones: string; oxigenacion: string } | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchData = async () =>{
+        try {
+            const response = await fetch(ESP32_URL);
+            if (!response.ok) {
+              throw new Error('Error al obtener datos');
+            }
+            const jsonData = await response.json();
+            setData(jsonData);
+          } catch (err) {
+            setError("error");
+          }
+    }
+    useEffect(() => {
+        // Obtener datos inmediatamente al montar el componente
+        fetchData();
+        
+        // Configurar intervalo para obtener datos cada 2 minutos (120000 ms)
+        const interval = setInterval(() => {
+          fetchData();
+        }, 120000);
+    
+        // Limpiar el intervalo al desmontar el componente
+        return () => clearInterval(interval);
+      }, []);
+
     return(
         <View style={styles.content}>
-            <TextHolder title="Pulsaciones" description="85" />
-            <TextHolder title="Oxigenación" description="522"/>
+            {error ? (
+                <View> 
+                    <TextHolder title="Pulsaciones" description="0" />
+                    <TextHolder title="Oxigenación" description="0"/>
+                </View>
+                
+            ):(
+                data &&(
+                    <View > 
+                    <TextHolder title="Pulsaciones" description={data.pulsaciones} />
+                    <TextHolder title="Oxigenación"  description={data.oxigenacion}/>
+                    </View>
+            ))}
+            
         </View>
     )
 }
