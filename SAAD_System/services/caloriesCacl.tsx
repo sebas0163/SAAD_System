c7ad3c655d1b3b742dd08b1604114f34c38649db
Sebas <sebas0163@estudiantec.cr>
2024-10-21 17:06:29 -0600

    [Add] Training Zones View

--- SAAD_System/services/caloriesCacl.tsx
+++ SAAD_System/services/caloriesCacl.tsx
@@ -25,3 +25,22 @@ export  function CalcCaloriesDay(mode:number, time:number, weight:number){
     cal = Math.round((time/60) *((met * weight))/60);
     return cal
 }
+export function trainingZones(age:number,heartRate:number[]){
+    let fcm = 220- age // Max heartrate frecuency
+    let porcent:number[] = [0,0,0];
+    heartRate.forEach((rate)=>{
+        let range:number = (rate*100)/fcm
+        if (range>50 && range <70){
+            porcent[0] +=1;
+        }else if (range > 70 && range < 85){
+            porcent[1] +=1;
+        }else if(range> 85){
+            porcent[2] += 1;
+        }
+    });
+    let result:number[] =[Math.round((porcent[0]*100)/heartRate.length),Math.round((porcent[1]*100)/heartRate.length),Math.round((porcent[2]*100)/heartRate.length)];
+    return result
+
+
+
+}
