d12851d4e42474e0cda8ac18cf98be3f67ad76f7
Sebas <sebas0163@estudiantec.cr>
2024-10-21 19:18:22 -0600

    [Add] connection verification

--- SAAD_System/components/templates/index.tsx
+++ SAAD_System/components/templates/index.tsx
@@ -6,21 +6,42 @@ import { StatusBar } from "expo-status-bar";
 import Title from '@/components/atoms/title';
 import { useRouter } from "expo-router";
 import { databaseController } from "@/services/firebase";
+const ESP32_URL = 'http://192.168.100.13:80/connect';
 
 const ini= require("@/assets/images/home.jpg");
 
 export function Index(){
-    const database = new databaseController();
     const router= useRouter();
+    const fetchData = async () => {
+        try {
+          const response = await fetch(ESP32_URL);
+          if (!response.ok) {
+            Alert.alert("Estado de la conexión","No se ha podido establecer la conexión");
+            throw new Error('Error al obtener conectar');
+            
+          }
+          const jsonData = await response.json();
+          if(jsonData.connect =="1"){
+            Alert.alert("Estado de la conexión","Conectado con éxito!!");
+            router.navigate('/(tabs)/home');
+          }else{
+            Alert.alert("Estado de la conexión","No se ha podido establecer la conexión");
+          }
+      }catch (err) {
+        console.log("Error al obtener datos");
+        Alert.alert("Estado de la conexión","No se ha podido establecer la conexión");
+      }
+    }
+
+
+
     return(
         <View style={styles.container}>
             <ImageBackground source={ini} resizeMode="cover" style={styles.image}>
                 <StatusBar style="light" backgroundColor="#6a51ae" />
                 <Title text="SAAD Aplication" />
                 <CenterText text_="Para iniciar con la Aplicación SAAD por favor vincule su dispositivo." />
-                <Button_atom text="Establecer Conexión" onPress={()=>{Alert.alert("Estado de la conexión","Conectado con éxito!!");
-                    router.navigate('/(tabs)/home');
-                }} /> 
+                <Button_atom text="Establecer Conexión" onPress={()=>{fetchData()}} /> 
                 {/*agregar aqui arriba el método de conexión */}
             </ImageBackground>
             
